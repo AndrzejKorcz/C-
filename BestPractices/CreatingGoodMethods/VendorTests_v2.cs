6cfed186bb77aef5b2612d6deb2ef170673b5bc9
Andrzej <akorcz74@gmail.com>
2018-10-09 12:37:56 +0200

    Modified Vendor_v2.cs file

--- BestPractices/CreatingGoodMethods/VendorTests_v2.cs
+++ BestPractices/CreatingGoodMethods/VendorTests_v2.cs
@@ -1,13 +1,12 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
-using Acme.Biz;
+using ConsoleApp2;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Acme.Common;
 
-namespace Acme.Biz.Tests
+namespace ConsoleApp2.Tests
 {
     [TestClass()]
     public class VendorTests
@@ -63,29 +62,11 @@ namespace Acme.Biz.Tests
             // Arrange
             var vendor = new Vendor();
             var product = new Product(1, "Saw", "");
-            var expected = new OperationResult(true, 
+            var expected = new OperationResult(true,
                 "Order from Acme, Inc\r\nProduct: Tools-1\r\nQuantity: 12");
 
             // Act
             var actual = vendor.PlaceOrder(product, 12);
-
-            // Assert
-            Assert.AreEqual(expected.Success, actual.Success);
-            Assert.AreEqual(expected.Message, actual.Message);
-        }
-        [TestMethod()]
-        public void PlaceOrder_3Parameters()
-        {
-            // Arrange
-            var vendor = new Vendor();
-            var product = new Product(1, "Saw", "");
-            var expected = new OperationResult(true,
-                "Order from Acme, Inc\r\nProduct: Tools-1\r\nQuantity: 12" +
-                "\r\nDeliver By: 10/25/2015");
-
-            // Act
-            var actual = vendor.PlaceOrder(product, 12,
-                new DateTimeOffset(2015, 10, 25, 0, 0, 0, new TimeSpan(-7, 0, 0)));
 
             // Assert
             Assert.AreEqual(expected.Success, actual.Success);
