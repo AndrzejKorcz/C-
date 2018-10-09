10c85d09c563f9969ca6097afbeb009735b2df2b
Andrzej <akorcz74@gmail.com>
2018-10-09 10:31:49 +0200

    Added Vendor_v1.cs file

---
+++ BestPractices/CreatingGoodMethods/Vendor_v1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    public class Vendor
+    {
+        public int VendorId { get; set; }
+        public string CompanyName { get; set; }
+        public string Email { get; set; }
+
+        public bool PlaceOrder(Product product, int quantity)
+        {
+            if (product == null)
+                throw new ArgumentNullException(nameof(product));
+            if (quantity <= 0)
+                throw new ArgumentOutOfRangeException(nameof(quantity));
+
+            var success = false;
+
+            var orderText = "Order from Acme, Inc" + System.Environment.NewLine +
+                           "Product: " + product.ProductCode +
+                                                   System.Environment.NewLine +
+                           "Quantity: " + quantity;
+
+            var emailService = new EmailService();
+            var confirmation = emailService.SendMessage("New Order", orderText,
+                                                                     this.Email);
+            if (confirmation.StartsWith("Message sent:"))
+            {
+                success = true;
+            }
+
+            return success;
+        }
+
+        public string SendWelcomeEmail(string message)
+        {
+            var emailService = new EmailService();
+            var subject = ("Hello " + this.CompanyName).Trim();
+            var confirmation = emailService.SendMessage(subject,
+                                                        message,
+                                                        this.Email);
+            return confirmation;
+        }
+    }
+}
